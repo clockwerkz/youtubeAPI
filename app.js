5cb0c18bf06867660a21b50a155808b3efd34d23
clockwerkz <clockwerkz@gmail.com>
2020-02-15 14:44:06 -0800

    added text doc with all the video names/dates

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const fs = require('fs');
 
 async function pullData() {
 	let count = 0;
-	let page = '';
+	let page = 'CO4FEAA';
 	let entries = [];
 	do {
+		console.log(count);
+		try {
 		const endpoint = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCpKV3InanmihC8do0XRoBkQ&maxResults=50&order=date&pageToken=${page}&key=${process.env.key}`;
 		let res = await axios.get(endpoint);
 		page = res.data.nextPageToken;
@@ -15,11 +17,14 @@ async function pullData() {
 			return `${date},${entry.snippet.title}\r\n`;
 		});
 		entries = entries.concat(arr);
+		} catch (err) {
+			console.log(err);
+		}
 		count++;
-	} while (count < 56 && page!=='');
+	} while (count < 40 && page!=='');
 	let str = entries.reverse().join('');
 	console.log(page);
-	fs.writeFile('beyond1031.csv', str, (err) => console.log('error writing file:', err));
+	fs.writeFile('beyond1031_a.csv', str, (err) => console.log('error writing file:', err));
 	//nextPageToken
 }
 pullData();
